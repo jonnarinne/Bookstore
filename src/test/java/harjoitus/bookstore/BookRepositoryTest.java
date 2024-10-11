af30b6d10fd9458a51ad54d3084a2b6e6247ccf8
Jonna Rinne <jonna.rinne1@gmail.com>
2024-10-11 13:24:11 +0300

    JPA-testi lisätty

--- src/test/java/harjoitus/bookstore/BookRepositoryTest.java
+++ src/test/java/harjoitus/bookstore/BookRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.junit.jupiter.api.Test;
@@ -18,27 +17,74 @@ import harjoitus.bookstore.domain.CategoryRepository;
 public class BookRepositoryTest {
 
     @Autowired
-	private CategoryRepository crepository; 
+    private CategoryRepository crepository; 
 
-	@Autowired
-	private BookRepository bookRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
+    // Testi uuden kirjan luomiselle
     @Test
     public void createNewBook() {
-    	Category category = new Category("Scifi");
-    	crepository.save(category);
-    	Book book = new Book("Kirja", "Kirjailija", 2024, 1111, 30);
-    	bookRepository.save(book);
-    	assertThat(book.getId()).isNotNull();
+        // Luodaan uusi kategoria ja tallennetaan se tietokantaan
+        Category category = new Category("Scifi");
+        crepository.save(category);
+
+        // Luodaan uusi kirja ja asetetaan kategoria
+        Book book = new Book("Kirja", "Kirjailija", 2024, 1111, 30);
+        book.setCategory(category); // Asetetaan kategoria
+        bookRepository.save(book);
+
+        // Varmistetaan, että kirja sai tietokantaan tallennuksen jälkeen ID:n
+        assertThat(book.getId()).isNotNull();
     }
-    
+
+    // Testi olemassa olevan kirjan kategorian lisäämiselle
     @Test
-    public void deleteNewBook() {
-		List<Book> books = bookRepository.findByTitle("Harry Potter");
-		Book book = books.get(0);
-		bookRepository.delete(book);
-		List<Book> newBooks = bookRepository.findByTitle("Harry Potter");
-		assertThat(newBooks).hasSize(0);
-     }
-    
+    public void addCategoryToExistingBook() {
+        // Luodaan uusi kategoria ja tallennetaan se tietokantaan
+        Category category = new Category("Fantasy");
+        crepository.save(category);
+
+        // Oletetaan, että tietokannassa on jo olemassa kirja ilman kategoriaa
+        Book existingBook = new Book("Harry Potter", "J.K. Rowling", 1997, 12345, 20);
+        bookRepository.save(existingBook); // Tallennetaan kirja ilman kategoriaa
+
+        // Lisätään kategoria olemassa olevalle kirjalle
+        existingBook.setCategory(category);
+        bookRepository.save(existingBook); // Päivitetään kirja
+
+        // Haetaan kirja ja varmistetaan, että kategoria on lisätty
+        Book updatedBook = bookRepository.findById(existingBook.getId()).orElse(null);
+        assertThat(updatedBook).isNotNull();
+        assertThat(updatedBook.getCategory()).isEqualTo(category); // Varmistetaan, että kategoria on sama
+    }
+
+    // Testi kirjan poistamiselle
+    @Test
+    public void deleteBook() {
+        // Luodaan kategoria ja lisätään "Harry Potter" -kirja testitietokantaan
+        Category category = new Category("Fantasy");
+        crepository.save(category);
+        
+        // Luodaan "Harry Potter" -kirja ja asetetaan kategoria
+        Book book = new Book("Harry Potter", "J.K. Rowling", 1997, 12345, 20);
+        book.setCategory(category); // Asetetaan kategoria
+        bookRepository.save(book);
+
+        // Haetaan kirja otsikon perusteella (tämän pitäisi palauttaa lista)
+        List<Book> books = bookRepository.findByTitle("Harry Potter");
+
+        // Tarkistetaan, että ainakin yksi kirja löytyi ennen kuin jatketaan
+        assertThat(books).isNotEmpty();
+
+        // Otetaan ensimmäinen löytynyt kirja
+        Book bookToDelete = books.get(0);
+
+        // Poistetaan kirja tietokannasta
+        bookRepository.delete(bookToDelete);
+
+        // Haetaan kirja uudelleen ja varmistetaan, että sitä ei enää ole tietokannassa
+        List<Book> newBooks = bookRepository.findByTitle("Harry Potter");
+        assertThat(newBooks).hasSize(0);
+    }
 }
